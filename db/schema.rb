0ec0ae896dd6efea4fd7dc2079f2ae012327427d
Ryan O'Connor <charle692@gmail.com>
2014-11-23 17:57:54 -0500

    Added user controller and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141123224414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: true do |t|
+    t.string   "fName"
+    t.string   "lName"
+    t.string   "email"
+    t.string   "typeOfAccount"
+    t.integer  "maxRent"
+    t.date     "creationDate"
+    t.time     "creationTime"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "password_digest"
+  end
 
 end
