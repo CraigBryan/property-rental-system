69863d65dd66b826c183017de4d3eab345899fbb
Kat Drobnjakovic <kat.drobnjakovic@gmail.com>
2014-11-27 13:24:33 -0500

    Added customer and owner to the session helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,5 +6,21 @@ module SessionsHelper
  		current_user.is_role_by_name?("admin")
  	end
  end
+
+ def customer?
+ 	if current_user.nil?
+ 		false
+ 	else
+ 		current_user.is_role_by_name?("customer")
+ 	end
+ end
+
+ def owner?
+ 	if current_user.nil?
+ 		false
+ 	else
+ 		current_user.is_role_by_name?("owner")
+ 	end
+ end
  
 end
