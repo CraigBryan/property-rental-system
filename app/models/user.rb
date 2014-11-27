3f612541b9d33f6d684b485b6efa75f847cb750f
Kat Drobnjakovic <kat.drobnjakovic@gmail.com>
2014-11-27 01:15:44 -0500

    added many to many relationship between users and roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,34 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_and_belongs_to_many :roles
+
+  def add_role(role)
+  	roles << role 
+  end
+
+  def add_role_by_name(role_name)
+  	role = Role.find_by(name: role_name)
+  	add_role(role)
+  end
+
+  def remove_role(role)
+  	roles.delete(role)
+  end
+
+  def remove_role_by_name(role_name)
+  	role = Role.find_by(name: role_name)
+  	remove_role(role)
+  end
+
+  def is_role?(role) 
+  	roles.include?(role) 
+  end
+
+  def is_role_by_name?(role_name)
+  	role = Role.find_by(name: role_name)
+  	is_role?(role)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141127060816) do
+ActiveRecord::Schema.define(version: 20141127061354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20141127060816) do
 
   create_table "roles_users", id: false, force: true do |t|
     t.integer "user_id"
+    t.integer "role_id"
   end
 
   create_table "users", force: true do |t|
