42336b84c164beb346733b57333cd2805c4efb7b
Craig <craigbryan.csb@gmail.com>
2014-11-27 00:09:47 -0500

    Added a location model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141125042808) do
+ActiveRecord::Schema.define(version: 20141127050231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 20141125042808) do
   end
 
   add_index "leases", ["property_id"], name: "index_leases_on_property_id", using: :btree
+
+  create_table "locations", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "payments", force: true do |t|
     t.string   "credit_card_type"
