dca66a0943b277798ae839ef042edca6f6c3536f
Kat Drobnjakovic <kat.drobnjakovic@gmail.com>
2014-11-27 22:59:51 -0500

    Implemented index, show, new, create, and destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,38 @@
 class UsersController < ApplicationController
+  before_action :deny_access_for_non_admins, except:[:index,:show] 
+  
   def index
+  	@users = User.all
   end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save 
+      redirect_to @user
+    else
+      # DO SOMETHING ELSE
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to user_path #redirects to index
+  end
+
+
+  private
+
+    def user_params
+      params.require(:user).permit(:email,:password, :password_confirmation) 
+    end
+
 end
