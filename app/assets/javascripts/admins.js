9dfbf60b7af318fac6e6e98d71a003ea59d19860
Kat Drobnjakovic <kat.drobnjakovic@gmail.com>
2014-11-29 17:18:41 -0500

    Added max rent to the form for only customer users

--- app/assets/javascripts/admins.js
+++ app/assets/javascripts/admins.js
@@ -1,14 +1,27 @@
 
 var ready;
 ready = function() {
-	alert("HELLLOO");
+	console.log("HELLLOO");
 
-  	if (document.getElementById("1").checked == true) {
-		document.getElementById("user_max_rent").style.display = 'block';
+	function disable_rent() {
+		if(document.getElementById('user_max_rent').style.display=='block') 
+		{
+      		document.getElementById('user_max_rent').style.display='none';
+    	}
 	}
-	else () {
-		document.getElementById("user_max_rent").style.display = 'none';
+
+	function enable_rent() {
+		if(document.getElementById('user_max_rent').style.display=='none') 
+		{
+      		document.getElementById('user_max_rent').style.display='block';
+    	}
+	
 	}
+
+	$("#radio0").click(disable_rent);
+	$("#radio1").click(enable_rent);
+	$("#radio2").click(disable_rent);
+
 };
 
 jQuery(document).ready(ready);
