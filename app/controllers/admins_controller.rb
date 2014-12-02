84eec3cc980d2ae1d9d03a3cc200811b2e5e60be
Kat Drobnjakovic <kat.drobnjakovic@gmail.com>
2014-12-01 19:59:45 -0500

    Fixed create user form validation

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,6 +1,8 @@
 class AdminsController < ApplicationController
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :deny_access_for_non_admins
+
+
   
   def new_user_by_admin
     if params[:user].nil?
@@ -15,32 +17,35 @@ class AdminsController < ApplicationController
     @user.add_role_by_name(Role.find(params[:role][:roles]).name)
     
     @user.valid?
-
+    #puts @user.valid?
+    #puts @user.errors.full_messages
 
     user_name_exist = User.exists? user_name: params[:user][:user_name]
     password_match = params[:user][:password] != params[:user][:password_confirmation]
-    user_name_invalid = params[:user_name].to_s =~ (/[^a-zA-Z0-9]/)
+    user_name_invalid = ((/[^0-9A-Za-z]/) =~ params[:user][:user_name])
 
     prefix = ['Oh Snap!', 'Holy Moly!', 'Jeepers!', 'Gosh Darnit!'].sample
 
     case 
     when user_name_exist && password_match
-      flash[:unsuccessful_signup] = prefix << " That username is already taken and you're passwords don't match"
+      flash[:alert] = prefix << " That username is already taken and you're passwords don't match"
       render :new_user_by_admin
     when user_name_exist
-      flash[:unsuccessful_signup] = prefix << " That username is already taken."
+      flash[:alert] = prefix << " That username is already taken."
       render :new_user_by_admin
     when password_match
-      flash[:unsuccessful_signup] = prefix << " You're passwords don't match."
+      flash[:alert] = prefix << " You're passwords don't match."
       render :new_user_by_admin
-    when user_name_invalid
-      flash[:invalid_username] = prefix << " Usernames can only contain letters and numbers." 
+    when user_name_invalid 
+      flash[:alert] = prefix << " Username can only contain letters and numbers." 
       render :new_user_by_admin
     when @user.save
-      flash[:successful_signup] = prefix << " Account created!"
+      flash[:notice] = " Account created!"
       redirect_to root_path
     else
-      flash[:unsuccessful_signup] = prefix << " Fix up your form"
+      flash[:alert] = prefix << " Ensure the username only contains numbers and letters. The email must be a valid email 
+        of the type opr@opr.com. A role must be selected. The user's first name and last name must be specified. A customer's max rent 
+        must be completed with a number greated than 0. Passwords must match."
       render :new_user_by_admin
     end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :roles
 
   attr_accessor :login
-
+  
+  validates :user_name, presence: true
   validates :email, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
